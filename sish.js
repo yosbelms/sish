80c7eb2ada82e53dff35418865e0f911d03ac2ce
Yosbel Marin <yosbel.marin@gtm.jovenclub.cu>
2016-11-03 16:46:31 -0600

    remove the use of multiple sources, and add the `import` internal function to achieve the same functionality.

--- sish.js
+++ sish.js
@@ -7,27 +7,30 @@
 
 var slice = Array.prototype.slice;
 
-// return a caller binded to a `src` object
-// if no `src` is provided `this` will be used instead
-function sish(src) {
-    var source = {};
+// return a caller function binded to a `src` object
+// if no `src` is provided `this` will be used instead,
+// if many `src` are provided all functions will be
+// available in the internal `scope` object which will
+// receive all properties of the passed `src`
+function Sish(src) {
+    var prop, scope;
 
-    slice.call(arguments).forEach(function(src) {
-        Object.keys(src).forEach(function(name) {
-            source[name] = (typeof fn === 'function') ?
-                src[name].bind(src) :
-                src[name];
-        })
-    })
+    scope = {
+        'import': function(src, names) {
+            _import(scope, src, names);
+        }
+    };
+
+    _import(scope, src);
 
     // call(Function, args...)
     // executes a function using the provided arguments
     // call(String, args...)
-    // finds a property with such name in the `source` object and executes it
+    // finds a property with such name in the `scope` object and executes it
     return function call(fname) {
         var
         fn = (typeof fname === 'string') ?
-            source[fname] :
+            scope[fname] :
             fname;
 
         if (typeof fn !== 'function')
@@ -39,10 +42,20 @@ function sish(src) {
     }
 }
 
+function _import(scope, src, names) {
+    names = names || Object.keys(src);
+    names.forEach(function(name) {
+        prop = src[name];
+        scope[name] = (typeof prop === 'function') ?
+            prop.bind(src) :
+            prop;
+    })
+}
+
 // publish
 if (typeof exports === 'object')
-    module.exports = sish;
+    module.exports = Sish;
 else
-    this.sish = sish;
+    this.Sish = Sish;
 
 }.call(this))
